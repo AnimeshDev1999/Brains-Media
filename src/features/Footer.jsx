8fd78487419823c6675977ebf6f68d416dbd1b6c
Animesh Kumar <animesh99dev@gmail.com>
2023-11-26 00:02:49 +0530

    finished site

--- src/features/Footer.jsx
+++ src/features/Footer.jsx
@@ -5,15 +5,38 @@ import linked from "../images/linked.svg";
 import insta from "../images/insta.svg";
 
 const Footer = () => {
+  const data = [
+    {
+      img: insta,
+      alt: "instagram",
+      link: "https://www.instagram.com/Brains_Media_Solutions/",
+    },
+    {
+      img: fb,
+      alt: "facebook",
+      link: "https://www.facebook.com/brainsmediasolutions",
+    },
+    {
+      img: tw,
+      alt: "twitter",
+      link: "https://twitter.com/i/flow/login?redirect_after_login=%2Fbrainsmedia2012",
+    },
+    {
+      img: linked,
+      alt: "linkedin",
+      link: "https://www.linkedin.com/company/brains-media-solutions-pvt-ltd-/about/",
+    },
+  ];
   return (
     <div className="bg-mlite p-5 mt-32">
       <div className="grid max-w-[1280px] mx-auto grid-cols-2 md:place-items-center md:grid-cols-3 gap-y-5 pb-5 border-b-mgld border-b-[2px]">
         <img src={logo} alt="logo" className="h-16" />
         <div className="flex gap-3">
-          <img src={insta} alt="facebook" className="w-7" />
-          <img src={fb} alt="facebook" className="w-7" />
-          <img src={tw} alt="facebook" className="w-7" />
-          <img src={linked} alt="facebook" className="w-7" />
+          {data.map((item) => (
+            <a target="_blank" href={item.link}>
+              <img src={item.img} alt={item.alt} className="w-7" />
+            </a>
+          ))}
         </div>
         <div className="col-span-2 md:col-span-1">
           <a href="tel:95355-33588" className="block text-lg">
